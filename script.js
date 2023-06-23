80da0f4809ea2f15f54ce85df9dc65a616437abb
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2023-06-23 15:53:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function exportAsPDF() {
   html2canvas(element, { allowTaint: true }).then(function(canvas) {
     var imageData = canvas.toDataURL("image/png");
 
-    pdf.addImage(imageData, "PNG", 10, 10, 50, 50); // Add Niedersachsen logo as header
+    pdf.addImage(imageData, "PNG", 10, 10, 50, 20); // Angepasste Größe für das Niedersachsen-Logo im Header
 
     pdf.setPageHeight(canvas.height + 40); // Add space for header and footer
     pdf.addPage();
@@ -14,7 +14,7 @@ function exportAsPDF() {
     pdf.addImage(imageData, "PNG", 40, pdf.internal.pageSize.getHeight() - 30, 20, 20); // Add second QR code as footer
 
     pdf.addPage();
-    pdf.addImage(imageData, "PNG", 10, 10, canvas.width / 2, canvas.height / 2);
+    pdf.addImage(imageData, "PNG", 10, 10, canvas.width / 2, canvas.height / 2); // Angepasste Größe des Bildes auf der zweiten Seite
 
     pdf.save("dashboard.pdf");
   });
