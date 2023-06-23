46d953fade4eb0ccba68b5c5b9d6db2a7bb41e4c
christoph-LSN <94443931+christoph-LSN@users.noreply.github.com>
2023-06-23 16:52:51 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,38 +1,26 @@
-// Funktion zum Exportieren der Seite als PDF
 function exportAsPDF() {
   var element = document.getElementById('pdf-export');
 
   html2canvas(element).then(function(canvas) {
     var imgData = canvas.toDataURL('image/png');
-    var pdf = new jsPDF(); // Verwende die richtige Groß- und Kleinschreibung für jsPDF
+    var pdf = new jsPDF();
 
-    pdf.addImage(imgData, 'PNG', 10, 10, 190, 277);
+    // Seitenverhältnis des PDFs
+    var pdfAspectRatio = pdf.internal.pageSize.getWidth() / pdf.internal.pageSize.getHeight();
 
-    // Tabelle exportieren
-    var tableElement = document.getElementById('data-table1');
-    var tableImgData = tableToImage(tableElement);
-    pdf.addPage();
-    pdf.addImage(tableImgData, 'PNG', 10, 10);
+    // Seitenverhältnis des Canvas-Elements
+    var canvasAspectRatio = canvas.width / canvas.height;
 
+    var imgWidth, imgHeight;
+    if (canvasAspectRatio < pdfAspectRatio) {
+      imgWidth = pdf.internal.pageSize.getWidth();
+      imgHeight = canvas.height * imgWidth / canvas.width;
+    } else {
+      imgHeight = pdf.internal.pageSize.getHeight();
+      imgWidth = canvas.width * imgHeight / canvas.height;
+    }
+
+    pdf.addImage(imgData, 'PNG', 0, 0, imgWidth, imgHeight);
     pdf.save('export.pdf');
   });
-}
-
-// Hilfsfunktion zum Umwandeln einer Tabelle in ein Bild (Canvas-Daten-URL)
-function tableToImage(tableElement) {
-  var tableCanvas = document.createElement('canvas');
-  tableCanvas.width = tableElement.offsetWidth;
-  tableCanvas.height = tableElement.offsetHeight;
-
-  var tableContext = tableCanvas.getContext('2d');
-  tableContext.fillStyle = '#fff';
-  tableContext.fillRect(0, 0, tableCanvas.width, tableCanvas.height);
-
-  var tableImgData;
-  html2canvas(tableElement).then(function(canvas) {
-    tableContext.drawImage(canvas, 0, 0);
-    tableImgData = tableCanvas.toDataURL('image/png');
-  });
-
-  return tableImgData;
 }
